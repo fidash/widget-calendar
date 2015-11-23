13a12204a9b60cd5bcb16ff050247cddd9a162a1
Javier Ortiz Sancho <jortiz@conwet.com>
2015-11-23 19:23:07 +0100

    Add scrollKey

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@
     "use strict";
 
     var calendar = new Calendar();
-    calendar.init(document.getElementById('visualization'),{});
+    calendar.init(document.getElementById('visualization'),
+    {
+      height: "100vh", 
+      orientation: "top",
+      zoomKey: 'shiftKey'
+    });
 
 })();
