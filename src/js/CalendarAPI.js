aafbbfb088e264668c800ba70080bafeacff5ba7
Javier Ortiz Sancho <jortiz@conwet.com>
2016-01-08 12:39:40 +0100

    Add auxiliar method

--- src/js/CalendarAPI.js
+++ src/js/CalendarAPI.js
@@ -17,11 +17,11 @@ var CalendarAPI = (function () {
     /*                P R I V A T E   F U N C T I O N S               */
     /******************************************************************/
     function getAllEvents(success, error) {
-        MashupPlatform.http.makeRequest(url + "/events", {
+      MashupPlatform.http.makeRequest(url + "/events", {
         method: 'GET',
         requestHeaders: {
-        "X-FI-WARE-OAuth-Token": "true",
-        "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
+          "X-FI-WARE-OAuth-Token": "true",
+          "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
         },
         onSuccess: success,
         onError: error
@@ -29,12 +29,13 @@ var CalendarAPI = (function () {
     }
     
     function addEvent(event, success, error) {
+      console.log(JSON.stringify(event));
         MashupPlatform.http.makeRequest(url + "/events", {
         method: 'POST',
         contentType: "application/json",
         requestHeaders: {
-        "X-FI-WARE-OAuth-Token": "true",
-        "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
+          "X-FI-WARE-OAuth-Token": "true",
+          "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
         },
         postBody: JSON.stringify(event),
         onSuccess: success,
@@ -43,7 +44,7 @@ var CalendarAPI = (function () {
     }
     
     function deleteEvent(event, success, error) {
-        MashupPlatform.http.makeRequest(url + "/events/" + event.uid, {
+      MashupPlatform.http.makeRequest(url + "/events/" + event.uid, {
         method: 'DELETE',
         requestHeaders: {
         "X-FI-WARE-OAuth-Token": "true",
@@ -55,15 +56,27 @@ var CalendarAPI = (function () {
     }
     
     function test(event, success, error) {
-        MashupPlatform.http.makeRequest(url, {
+      MashupPlatform.http.makeRequest(url, {
         method: 'GET',
         requestHeaders: {
-        "X-FI-WARE-OAuth-Token": "true",
-        "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
+          "X-FI-WARE-OAuth-Token": "true",
+          "X-FI-WARE-OAuth-Header-Name": "X-Auth-Token"	
         },
         onSuccess: success,
         onError: error
       });
+    }
+    
+    function createEventObject(uid, description, summary, location, dtstart, dtend, dtstamp) {
+      var event = {};
+      if (uid !== null) event.uid = uid;
+      if (description !== null) event.description = description;
+      if (summary !== null) event.summary = summary;
+      if (location !== null) event.location = location;
+      if (dtstart !== null) event.dtstart = dtstart;
+      if (dtend !== null) event.dtend = dtend;
+      if (dtstamp !== null) event.dtstamp = dtstamp;
+      return event;
     }
 
     /******************************************************************/
@@ -71,18 +84,18 @@ var CalendarAPI = (function () {
     /******************************************************************/
 
     CalendarAPI.prototype = {
-        getAllEvents: function (success, error) {
-          getAllEvents(success, error);
-        },
-        addEvent: function (event, success, error) {
-          addEvent(event, success, error);
-        },
-        deleteEvent: function (event, success, error) {
-          deleteEvent(event, success, error);
-        },
-        test: function (success, error) {
-          test(success, error);
-        }
+      getAllEvents: function (success, error) {
+        getAllEvents(success, error);
+      },
+      addEvent: function (event, success, error) {
+        addEvent(event, success, error);
+      },
+      deleteEvent: function (event, success, error) {
+        deleteEvent(event, success, error);
+      },
+      test: function (success, error) {
+        test(success, error);
+      }
     };
 
     return CalendarAPI;
