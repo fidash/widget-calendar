ed0a00fbc595db6aaa423d105e957c377099d091
Javier Ortiz Sancho <jortiz@conwet.com>
2016-01-11 15:03:35 +0100

    Remove space

--- src/js/RegionAPI.js
+++ src/js/RegionAPI.js
@@ -3,7 +3,7 @@
 var RegionAPI = (function () {
     "use strict";
 
-    var url =  "http://130.206.84.4:1028/monitoring/regions/";
+    var url = "http://130.206.84.4:1028/monitoring/regions/";
 
     /*****************************************************************
     *                     C O N S T R U C T O R                      *
