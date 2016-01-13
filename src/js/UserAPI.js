3b03624daebb1362e78a98943fd8997adbfee966
Javier Ortiz Sancho <jortiz@conwet.com>
2016-01-13 17:02:59 +0100

    Add new functionality to UserAPI to manage user roles

--- src/js/UserAPI.js
+++ src/js/UserAPI.js
@@ -4,6 +4,11 @@ var UserAPI = (function () {
     "use strict";
 
     var url = "https://account.lab.fiware.org/user";
+    var USERROLES = {
+      DEMO: "Demo",
+      INFRASTRUCTUREOWNER: "InfrastructureOwner",
+      ANONYMOUS: "Anonymous"
+    };
 
     /*****************************************************************
     *                     C O N S T R U C T O R                      *
@@ -28,6 +33,39 @@ var UserAPI = (function () {
       });
     }
     
+    function isInfrastructureOwner (user, region) {
+      var index;
+      if(region) {
+        var FIDASHRegion = region + " FIDASH";
+        for (index = 0; index < user.organizations.length; index++) {
+          if (FIDASHRegion === user.organizations[index].name) {
+            return true;
+          }
+        }
+        return false;
+      } else {
+        for (index = 0; index < user.roles.length; index++) {
+          if (user.roles[index].name === USERROLES.INFRASTRUCTUREOWNER) {
+            return true;
+          }
+        }
+        return false;
+      }
+    }
+    
+    function isDemo (user) {
+      for (var index = 0; index < user.roles.length; index++) {
+        if (user.roles[index].name === USERROLES.DEMO) {
+          return true;
+        }
+      }
+      return false;
+    }
+    
+    function isAnonymous (user) {
+      return !isInfrastructureOwner(user) && !isDemo(user);
+    }
+    
     function test(success, error) {
 
     }
@@ -42,7 +80,28 @@ var UserAPI = (function () {
       },
 		  test: function (success, error) {
 				test(success, error);
-			}
+			},
+      ROLES: {
+        DEMO: USERROLES.DEMO,
+        INFRASTRUCTUREOWNER: USERROLES.INFRASTRUCTUREOWNER,
+        ANONYMOUS: USERROLES.ANONYMOUS
+      },
+      utils: {
+        isInfrastructureOwner: function (user, region) {
+          return isInfrastructureOwner(user, region);
+        },
+        isDemo: function (user) {
+          return isDemo(user);
+        },
+        isAnonymous: function (user) {
+          return isAnonymous(user);
+        },
+        getRole: function (user) {
+          if (isInfrastructureOwner(user)) return USERROLES.INFRASTRUCTUREOWNER;
+          if (isDemo(user)) return USERROLES.DEMO;
+          if (isAnonymous(user)) return USERROLES.ANONYMOUS;
+        }
+      }
     };
 
     return UserAPI;
