5d3b9f39f07752067408ed405d64871b45ffe71c
Javier Ortiz Sancho <jortiz@conwet.com>
2016-01-13 17:41:09 +0100

    Update log version

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -232,7 +232,7 @@ var Calendar = (function (vis) {
 
   Calendar.prototype = {
     init: function (calendarContainer, calendarOptions) {
-      console.log("Start Timeline v0.8.2");
+      console.log("Start Timeline v0.8.70");
       
       calendarAPI = new CalendarAPI();
       regionAPI = new RegionAPI();
