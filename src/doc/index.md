cfb32397f63de2742ca27f9a234989d4abb788bc
mgarcia <miguelglafuente@gmail.com>
2016-03-11 13:59:34 +0100

    Add ICAL and info

---
+++ src/doc/index.md
@@ -0,0 +1,13 @@
+# Introduction
+
+Calendar widget is a horizontal-based timeline, where each region is placed on a different row, plus one for no-maintennance requests. Users must have proper rights to create events on a certain row, by veing infrastructure owner or the region or by being designated as authorized for creating no-maintenance requests. Roles in grey color are not writtable by the user, whereas he does have permission to write on white ones. A regular lab user shall see all the calendar marked in grey.
+
+# Usage
+
+![Calendar](calendar.png)
+
+By double clicking on the specified row (whenerer user is authorized), a modal dialog appears to enter details. Textual description must be written here, but timing can be stablished back on the calendar. By clicking on an event that the user can modify, interface changes being able to delete it or, more important, drag edges to the desired values for modifying start and end date and time.
+
+![Calendar Edit](calendar_edit.png)
+
+Another interesting feature is the zooming function. By clicking shift key while using the scroll, horizontal zooming happens, allowing to select the time-frame to be shown. Calendar can be also dragged horizontally to move on the current zoom level. Zooming function ranges from years to hours level.
--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,59 @@
-/* global console, Calendar, vis */
+/* global $, console, Calendar, vis, MashupPlatform */
 
 (function() {
     "use strict";
 
     var calendar = new Calendar();
-    calendar.init(document.getElementById('visualization'),
-    {
-      height: "100vh", 
-      orientation: "top",
-      zoomKey: 'shiftKey',
-      zoomMax: 315360000000,
-      zoomMin: 86400000,
-      editable: {
-        add: false,
-        updateTime: true,
-        updateGroup: false,
-        remove: true
-      },
-      groupOrder: function (a, b) {
-          if (a.id === "Demos") {
-            return -1;
-          }
-          if (b.id === "Demos") {
-            return 1;
-          }
-          
-          var groups = [a.id, b.id];
-          groups.sort();
-          if (a.id === groups[0]) {
-            return -1;
-          } else {
-            return 1;
-          }
-      }
+    calendar.init(document.getElementById('visualization'), {
+        height: "100vh",
+        orientation: "top",
+        zoomKey: 'shiftKey',
+        zoomMax: 315360000000,
+        zoomMin: 86400000,
+        editable: {
+            add: false,
+            updateTime: true,
+            updateGroup: false,
+            remove: true
+        },
+        groupOrder: function (a, b) {
+            if (a.id === "Demos") {
+                return -1;
+            }
+            if (b.id === "Demos") {
+                return 1;
+            }
+
+            var groups = [a.id, b.id];
+            groups.sort();
+            if (a.id === groups[0]) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
     });
-    
+
     $(document).ready(function(){
-      $('.vis-center>.vis-content').on('scroll', function () {
-          $('.vis-left>.vis-content').scrollTop($(this).scrollTop());
-      });
-      $('.vis-left>.vis-content').on('scroll', function () {
-          $('.vis-center>.vis-content').scrollTop($(this).scrollTop());
-      });	
+        var info = MashupPlatform.widget.getVariable("info");
+
+        $('.vis-center>.vis-content').on('scroll', function () {
+            $('.vis-left>.vis-content').scrollTop($(this).scrollTop());
+        });
+
+        $('.vis-left>.vis-content').on('scroll', function () {
+            $('.vis-center>.vis-content').scrollTop($(this).scrollTop());
+        });
+
+
+        if (info.get() !== "false") {
+            $(".helptab").removeClass("hide");
+        }
+
+        $(".removeinfo").click(function() {
+            info.set("false");
+            $(".helptab").addClass("hide");
+        });
     });
 
 })();
